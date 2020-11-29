ba38a65a600792ae979bcb413d7c90dc7ac856fd
warlock <h.kwiatkowski@hotmail.com>
2020-11-29 18:45:57 +0100

    PyQt5 added

--- BDO_app/main.py
+++ BDO_app/main.py
@@ -1,83 +1,27 @@
-import csv
 import sys
-import math
-from modules.kut_nou import open_kutum_nouver as opk
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication
+from PyQt5.uic import loadUi
 
-from tkinter import *
-
-root = Tk()
-
-kutum = [] # [level, AP min, AP max, bonus AP]
-nouver = [] # [level, AP min AP max]
-bonus = [] # [AP, extra AP]
-
-class BDO_app:
-    """A class to manage the app."""
+class MainWindow(QMainWindow):
     def __init__(self):
-        """Initialize the app."""
-        global kutNouCalc
-        kutNouCalc = Button(root, text="K/N calculator", command=self._opk)
+        super(MainWindow, self).__init__()
+        loadUi("BDO_app/pyqt5_ui/mainWindow.ui",self)
 
-    def run_app(self):
-        """Main loop of the app."""
-        global kutNouCalc
-        kutNouCalc.grid(row=0, column=0)
-        root.mainloop()
+class KutumVsNouverWindow(QDialog):
+    pass
 
-    def _opk(self):
-        apLevel, kutLevel, nouLevel = self._getAll()
-        # kutLevel = self._getKutum()
-        # nouLevel = self._getNouver()
+# main
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget()
+mainwindow = MainWindow()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(800)
+widget.setFixedHeight(600)
+widget.show()
 
-        totalAP = opk(apLevel, kutLevel, nouLevel)
 
-    def _getAll(self):
-        global kutNouCalc
-        kutNouCalc.grid_forget()
-
-        apLevelLabel= Label(root, text="AP without offhand")
-        kutLevelLabel= Label(root, text="Kutum level")
-        nouLevelLabel= Label(root, text="Nouver level")
-
-        apE = Entry(root, width=15, borderwidth=5)
-        kutE = Entry(root, width=15, borderwidth=5)
-        nouE = Entry(root, width=15, borderwidth=5)
-
-        submit = Button(root, text="Submit", command=apE.get)
-        # kutSubmit = Button(root, text="Submit", command=kutE.get)
-        # nouSubmit = Button(root, text="Submit", command=nouE.get)
-
-        apLevelLabel.grid(row=0, column=0)
-        apE.grid(row=0, column=1)
-
-        kutLevelLabel.grid(row=1, column=0)
-        kutE.grid(row=1, column=1)
-
-        nouLevelLabel.grid(row=2, column=0)
-        nouE.grid(row=2, column=1)
-
-        submit.grid(row=3, column=1)
-
-        return apE.get(), kutE.get(), nouE.get()
-
-"""    def _getKutum(self):
-        global kutNouCalc
-        levelLabel= Label(root, text="Kutum level")
-        e = Entry(root, width=35, borderwidth=5)
-        submit = Button(root, text="Submit", command=e.get)
-        kutNouCalc.grid_forget()
-        levelLabel.grid(row=0, column=0)
-        e.grid(row=1, column=0)
-        submit.grid(row=2, column=0)
-        return e.get()
-
-    def _getNouver(self):
-        global kutNouCalc
-        levelLabel= Label(root, text="Nouver level")
-        e = Entry(root, width=35, borderwidth=5)
-        submit = Button(root, text="Submit", command=e.get)
-        kutNouCalc.grid_forget()
-        levelLabel.grid(row=0, column=0)
-        e.grid(row=1, column=0)
-        submit.grid(row=2, column=0)
-        return e.get()"""
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
